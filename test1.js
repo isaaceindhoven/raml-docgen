581fcdcdf352364fd61ac95e8c5801c3e6484cfa
Cas Eliëns <cas.eliens@gmail.com>
2016-09-07 10:04:47 +0200

    First template attempt

--- test1.js
+++ test1.js
@@ -2,31 +2,36 @@ var raml = require("raml-1-parser");
 var fs = require("fs");
 var path = require("path");
 var nunjucks = require("nunjucks");
+var specPath = path.resolve(__dirname, "spec");
 
-var fName = path.resolve(__dirname, "spec/c2s-api.raml");
+//var fName = path.resolve(specPath, "c2s-api.raml");
+var fName = path.resolve(specPath, "qander1.raml");
 var api = raml.loadApiSync(fName);
-var jsonString = JSON.stringify(api.toJSON(), null, 2);
-
 var apiJSON = api.toJSON();
+var jsonString = JSON.stringify(apiJSON, null, 2);
+//writeJson(jsonString);
 
-
-
-console.log(apiJSON.title);
-console.log(apiJSON.version);
+var endpointArray = [];
 
 apiJSON.resources.forEach(echoThing);
+
+var res = nunjucks.render('template.adoc', {
+  title: apiJSON.title,
+  version: apiJSON.version,
+  author: apiJSON.author,
+  author_email: apiJSON.authoremail,
+  endpoints: endpointArray
+});
+
+writeTemplate(res);
 
 // Echo path for an endpoint
 function echoThing(item) {
   // Only print path and methods if this endpoint actually has methods - otherwise skip
   if(item.methods != undefined && item.relativeUri != undefined) {
-    // Write current path
-    if(item.parentPath != undefined) console.log(item.parentPath + item.relativeUri);
-    else console.log(item.relativeUri);
-
-    item.methods.forEach(echoMethods);
+    endpointArray.push(item);
   }
-  console.log();
+
   // Item has children, output them too.
   if(item.resources != undefined) {
     var parent = item;
@@ -38,8 +43,12 @@ function echoThing(item) {
   }
 }
 
-function echoMethods(item) {
-  console.log(item.method.toUpperCase() + ": " + item.description);
+function writeTemplate(templateString) {
+  fs.writeFile("api.adoc", templateString, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+  })
 }
 
 // Write the interpreted RAML to file
