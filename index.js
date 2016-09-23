2cdcb6e21202098075bd0a7a896fab38b7d56723
Cas Eliëns <cas.eliens@gmail.com>
2016-09-23 10:34:48 +0200

    add example and expand flags

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const optionDefinitions = [
   { name: 'input', alias: 'i', type: String, defaultValue: 'spec.raml' },
   { name: 'template', alias: 't', type: String, defaultValue: 'default' },
   { name: 'style', alias: 's', type: String, required: false },
-  { name: 'debug', alias: 'd', type: Boolean }
+  { name: 'debug', alias: 'd', type: Boolean },
+  { name: 'examples', alias: 'e', type: Boolean },
+  { name: 'noExpand', alias: 'n', type: Boolean }
 ]
 const options = commandLineArgs(optionDefinitions)
 
@@ -19,7 +21,11 @@ var env = nunjucks.configure('templates/' + options.template);
 // Read API
 var fName = path.resolve(__dirname, options.input);
 var api = raml.loadApiSync(fName);
-var apiJSON = api.expand().toJSON();
+if(options.noExpand) {
+  var apiJSON = api.toJSON();
+} else {
+  var apiJSON = api.expand().toJSON();
+}
 
 // Recursively add parent URI variables and convert methods to UPPERCASE
 maintenance(apiJSON);
@@ -27,18 +33,17 @@ maintenance(apiJSON);
 if(options.debug) writeDebug(apiJSON);
 
 var res;
-// Render asciidoc
-if(options.style != undefined) {
-  res = env.render('template.adoc', {
-    api: apiJSON,
-    style: options.style
-  });
-}
-else {
-  res = env.render('template.adoc', {
-    api: apiJSON
-  });
-}
+console.log(options.style);
+console.log(options.template);
+console.log(options.debug);
+console.log(options.examples);
+console.log(options.noExpand);
+if(options.style != undefined) env.addGlobal("style", options.style);
+if(options.examples) env.addGlobal("examples", true);
+
+res = env.render('template.adoc', {
+  api: apiJSON
+});
 
 // Save asciidoc
 writeAsciidoc(res);
@@ -68,14 +73,6 @@ function nodeMaintenance(node, types) {
     node.methods.forEach(function(m){
       m.method = m.method.toUpperCase();
     });
-  }
-
-  //TODO: Make this work
-  // Assign node types
-  if(node.type != undefined) {
-    for(value in types[node.type]) {
-      node[value] = types[node.type][value];
-    }
   }
 
   // Node has children, output them too.
