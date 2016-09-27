8e0e34cfafebb10a274ec0fce4ffb3a388ebb84e
Cas Eliëns <cas.eliens@gmail.com>
2016-09-27 14:38:58 +0200

    config can now be read using -c flag

--- index.js
+++ index.js
@@ -2,35 +2,32 @@ var raml = require("raml-1-parser");
 var fs = require("fs");
 var path = require("path");
 var nunjucks = require("nunjucks");
+const commandLineArgs = require('command-line-args')
 // Could use json-schema-ref-parser for JSON schema parsing
 
-// Handle command line arguments
-const commandLineArgs = require('command-line-args')
-const optionDefinitions = [
-  { name: 'input', alias: 'i', type: String, defaultValue: 'spec.raml' },
-  { name: 'template', alias: 't', type: String, defaultValue: 'default' },
-  { name: 'style', alias: 's', type: String, required: false },
-  { name: 'debug', alias: 'd', type: Boolean },
-  { name: 'json', alias: 'j', type: Boolean },
-  { name: 'examples', alias: 'e', type: Boolean },
-  { name: 'noExpand', alias: 'n', type: Boolean },
-  { name: 'headerregex', alias: 'h', type: String, required: false },
-  { name: 'headerannotation', alias: 'a', type: String, required: false },
-  { name: 'schemalocation', alias: 'l', type: String, required: false }
+var requiredOptions = ['input', 'template'];
+
+var optionDefinitions = [
+  { name: 'input',            alias: 'i', type: String },
+  { name: 'template',         alias: 't', type: String },
+  { name: 'style',            alias: 's', type: String },
+  { name: 'debug',            alias: 'd', type: Boolean },
+  { name: 'json',             alias: 'j', type: Boolean },
+  { name: 'examples',         alias: 'e', type: Boolean },
+  { name: 'noExpand',         alias: 'n', type: Boolean },
+  { name: 'headerregex',      alias: 'h', type: String },
+  { name: 'headerannotation', alias: 'a', type: String },
+  { name: 'config',           alias: 'c', type: String }
 ]
-const options = commandLineArgs(optionDefinitions)
+
+const options = getOptions(optionDefinitions);
 
 if(options.debug) {
-  console.log("Debug:             " + options.debug);
-  console.log("Write JSON:        " + options.json);
-  console.log("Style:             " + options.style);
-  console.log("Template:          " + options.template);
-  console.log("Examples:          " + options.examples);
-  console.log("No expand:         " + options.noExpand);
-  console.log("Header Regex:      " + options.headerregex);
-  console.log("Header Annotation: " + options.headerannotation);
-  console.log("Schema Location:   " + options.schemalocation);
+  console.log("\tDEBUG INFO:")
+  console.log(JSON.stringify(options, null, 2));
 }
+
+verifyOptions(requiredOptions);
 
 if(options.headerregex != undefined) var headerRegexp = new RegExp(options.headerregex);
 
@@ -178,4 +175,30 @@ function writeSchema(schemaJSON) {
       return console.log(err);
     }
   });
+}
+
+function readConfig(path) {
+  var readOptionsJSON = fs.readFileSync(path, 'utf8');
+  return JSON.parse(readOptionsJSON);
+}
+
+function getOptions(optionDefinitions) {
+  var cmdOptions = commandLineArgs(optionDefinitions)
+  var returned = {};
+
+  if(cmdOptions.config != undefined) returned = readConfig(cmdOptions.config);
+
+  for(key in cmdOptions) {
+    returned[key] = cmdOptions[key];
+  }
+
+  return returned;
+}
+
+function verifyOptions(requiredOptions) {
+  var missing = []
+  for(i in requiredOptions) {
+    if(options[requiredOptions[i]] == undefined) missing.push(requiredOptions[i]);
+  }
+  if(missing.length > 0) throw "Missing properties: " + missing.join(",");
 }
